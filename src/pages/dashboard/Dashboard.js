6f34c9c23f6d05852fa75c49557309a95e7e7c2a
Vinay <itsvinayadav@gmail.com>
2023-05-30 22:31:35 +0530

    First commit

---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,52 @@
+import {React, useState} from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { getRequest, putRequest } from '../../services/api';
+import { setProfile } from '../../redux/slices/profileSlice';
+
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const profileData = useSelector((state) => state.profile.profile);
+  const [data,setData] = useState([])
+  const [progress, setProgress] = useState(0);
+
+  const getApiCall = async() => {
+    const dataApi = await getRequest('users', setProgress);
+    if(dataApi?.status === 200){
+      toast.success('Success');
+      dispatch(setProfile(dataApi.data));
+      setData(dataApi.data);
+    }else{
+      toast.error(dataApi.message);
+    }
+  };
+  
+
+  const postApiCall = async() => {
+    let person = {
+      title:'react',
+      body:'JSX'
+    }
+    const dataApi = await putRequest('posts/1', person, setProgress);
+    if(dataApi?.status === 200){
+      toast.success('Success');
+      dispatch(setProfile(dataApi.data));
+      setData(dataApi.data);
+    }else{
+      toast.error(dataApi.message);
+    }
+  };
+
+  return (
+    <div>
+      <p>Dashboard</p>
+        {<p>{data[0]?.name || profileData.title} {progress}</p>}
+        {<p>{profileData[0]?.email || profileData.body}</p>}
+      <button className='btn btn-info me-5' onClick={getApiCall}>Get Api call</button>
+      <button className='btn btn-info' onClick={postApiCall}>Post Api call</button>
+    </div>
+  )
+}
+
+export default Dashboard
